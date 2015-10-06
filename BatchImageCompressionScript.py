994d67527a39acc54ffc2f68b108ee1f9b3ba5ba
log2akshat <akshat-pg8@iiitmk.ac.in>
2015-10-06 09:00:32 +0530

    Added Support for Mac and support for creating target directory manually.

--- BatchImageCompressionScript.py
+++ BatchImageCompressionScript.py
@@ -18,6 +18,7 @@ import os
 import csv
 import logging
 import argparse
+import platform
 import subprocess
 
 # Command line argument validation functions...
@@ -29,10 +30,24 @@ def is_valid_directory(parser, arg):
         # File exists so return the directory
         return arg
 
+def is_target_directory(arg):
+    "Function for checking specfied directory exists or not."
+    if not os.path.isdir(arg):
+        print('The directory %s does not exist!, so creating it for you..' % arg)
+        try:
+        	subprocess.call(['mkdir', '-p', arg])
+        	return arg
+        except:
+        	parser.error('The directory {} does not exist and unable to create for you, please create it manually!'.format(arg))
+    else:
+        # File exists so return the directory
+        return arg
+	
+	
 ## =========> Command line arguments parsing -- starts <========= ##
 parser = argparse.ArgumentParser(description='Batch image conversion utility. For running this program you need to have imagemagick installed om your machine.')
 parser.add_argument('-s','--source_directory', help='Directory to read input files.', required=True, metavar='<Source Directory>', type=lambda x: is_valid_directory(parser, x))
-parser.add_argument('-t','--target_directory', help='Directory to save output files.', required=True, metavar='<Target Directory>', type=lambda x: is_valid_directory(parser, x))
+parser.add_argument('-t','--target_directory', help='Directory to save output files.', required=True, metavar='<Target Directory>', type=lambda x: is_target_directory(x))
 parser.add_argument('-q','--quality', help='Quality of the Image to reatin.', required=True, metavar='<Image Quality>')
 parser.add_argument('-f','--filename', help='Desired output file name.', required=True, metavar='<Output file names>')
 parser.add_argument('-l','--log_file', help='Path of the log file.', metavar='<Log File>')
@@ -92,35 +107,45 @@ quality = args.quality
 tmpFile = '/tmp/batchIM.txt'
 devnull = open('/dev/null', 'w')
 
-file = []
-filterCall = subprocess.Popen(['ls', filepath],stdout=subprocess.PIPE)
-#tailCmd = subprocess.Popen(['tail', '-n+2'], stdin=filterCall.stdout, stdout=subprocess.PIPE)
-sortCmd = subprocess.Popen(['sort', '--version-sort', '-f'], stdin=filterCall.stdout, stdout=subprocess.PIPE)
-#awkCmd = subprocess.Popen(['awk', '{print substr($0,index($0,$9))}'], stdin=tailCmd.stdout, stdout=subprocess.PIPE)
-#teeCmd = subprocess.Popen(['tee', tmpFile], stdin=awkCmd.stdout, stdout=devnull)
-teeCmd = subprocess.Popen(['tee', tmpFile], stdin=sortCmd.stdout, stdout=devnull)
-sortCmd.stdout.close()
+def processingFile():
+    '''Function to create a temporary file for processing.'''
+    file = []
+    filterCall = subprocess.Popen(['ls', filepath],stdout=subprocess.PIPE)
+    #tailCmd = subprocess.Popen(['tail', '-n+2'], stdin=filterCall.stdout, stdout=subprocess.PIPE)
+    if platform.system() == "Darwin":
+        sortCmd = subprocess.Popen(['sort', '-f'], stdin=filterCall.stdout, stdout=subprocess.PIPE)
+    elif platform.system() == "Linux":
+        sortCmd = subprocess.Popen(['sort', '--version-sort', '-f'], stdin=filterCall.stdout, stdout=subprocess.PIPE)
+    #awkCmd = subprocess.Popen(['awk', '{print substr($0,index($0,$9))}'], stdin=tailCmd.stdout, stdout=subprocess.PIPE)
+    teeCmd = subprocess.Popen(['tee', tmpFile], stdin=sortCmd.stdout, stdout=devnull)
+    sortCmd.stdout.close()
+    teeCmd.communicate()
 
-i = 0
-counter = 1
-fileName = ""
-pathfile = open(tmpFile, 'r')
-file = pathfile.read().splitlines()
-with open(tmpFile, 'r') as f:
+def main():
+    processingFile()
+    i = 0
+    counter = 1
+    fileName = ""
+    pathfile = open(tmpFile, 'r')
+    file = pathfile.read().splitlines()
+    with open(tmpFile, 'r') as f:
 	reader = csv.reader(f, delimiter=' ')
 	for row in reader:
-		pathRow = row[0:]
-		length = len(pathRow)
-		if i < length:
-			for x in pathRow:
-				fileName = fileName + " " + pathRow[i]
-				#print "i : " + str(pathRow[i])
-				i = i + 1
-		fileName = fileName.strip()
-		#print "pathRow : " + str(fileName) + " :: " + str(pathRow) + " :: " + str(length)
-		imgpath = filepath + "/" + fileName
-		print "Resizing " + imgpath + "..."
-		subprocess.call(['convert', imgpath, '-quality', quality, targetDir + '/' + outfileName + ' ' + str(counter) + '.jpg'])
-		i = 0
-		fileName = ""
-		counter = counter + 1
+            pathRow = row[0:]
+            length = len(pathRow)
+            if i < length:
+                for x in pathRow:
+                    fileName = fileName + " " + pathRow[i]
+                    #print "i : " + str(pathRow[i])
+                    i = i + 1
+            fileName = fileName.strip()
+            #print "pathRow : " + str(fileName) + " :: " + str(pathRow) + " :: " + str(length)
+            imgpath = filepath + fileName
+            logger.info("Resizing Image : %s.." % imgpath)
+            subprocess.call(['convert', imgpath, '-quality', quality, str(targetDir) + '/' + str(outfileName) + ' ' + str(counter) + '.jpg'])
+            i = 0
+            fileName = ""
+            counter = counter + 1
+
+
+main()
