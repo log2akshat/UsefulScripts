9057e2a5ae4e4b1743627a49955fc82d5317d1c0
log2akshat <akshat-pg8@iiitmk.ac.in>
2019-01-13 10:27:46 +0530

    Fixed logging-not-lazy

--- BatchImageCompressionScript.py
+++ BatchImageCompressionScript.py
@@ -144,7 +144,7 @@ def main():
                     i = i + 1
             fileName = fileName.strip()
             imgpath = filepath + fileName
-            LOGGER.info("Resizing Image : %s.." % imgpath)
+            LOGGER.info("Resizing Image : %s..", imgpath)
             if outfileName:
                 subprocess.call(['convert', imgpath, '-quality', quality, str(targetDir) + '/' + str(outfileName) + ' ' + str(counter) + '.jpg'])
             else:
