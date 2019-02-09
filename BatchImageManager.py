ee72fe49168b1610d589d550cf2bd08541d416c4
log2akshat <akshat-pg8@iiitmk.ac.in>
2019-02-10 00:22:40 +0530

    Fixed trailing-whitespace as per pylint standards.

--- BatchImageManager.py
+++ BatchImageManager.py
@@ -35,6 +35,7 @@ def is_valid_directory(parser, arg):
         # File exists so return the directory
         return arg
 
+
 def is_target_directory(arg):
     "Function for checking specfied directory exists or not."
     if not os.path.isdir(arg):
@@ -48,13 +49,14 @@ def is_target_directory(arg):
         # File exists so return the directory
         return arg
 
+
 def is_valid_option(parser, arg):
     "Function for checking option is on or off."
     if not (arg == 'on' or arg == 'off'):
         parser.error('{} is not a valid input for turning option on or off! Please specify \"on\" for turning the option on and \"off\" for turning it off.'.format(arg))
     else:
         return arg
-    
+
 
 def is_valid_loggingStatus(parser, arg):
     "Function for checking logging status is valid or not."
@@ -62,6 +64,7 @@ def is_valid_loggingStatus(parser, arg):
         parser.error('{} is not a valid input for turning logging on or off! Please specify \"on\" for turning logging on and \"off\" for turning logging off.'.format(arg))
     else:
         return arg
+
 
 ## =========> Command line arguments parsing -- starts <========= ##
 parser = argparse.ArgumentParser(description='[Purpose - This script is useful in a situation where we want to run a compression only on specific camera make but not on others and later want to do some manual image processing using tools like gimp on some camera make images and also at the same time we also want to maintain the sequence of the images based on their capturing time not based on the image modification.]\n\n\n**********************************************************************\nThis script will copy the images from the specified directory and all of its sub-directory to the target directory. The target directory will be the provided on the command line argument; but the final destination will be targetDir/CameraManufacurer__CameraModel/unixTimeStamp_CameraMake.JPG\nn**********************************************************************\nBatch Image Manager is an advaced version of the Batch Image Compression utility. For running this program you need to have exif tool and imagemagick installed on your machine. This program is used for mixing the various images taken from different camera make based on their capturing time in ascending order and rename each image on thir unix timestamp.\n\nIf compression option is selected as OFF then this script wil not create any sub-directories based on camera make just it will arrange the pictures based on their capturing time. \n\n\n\t', formatter_class=RawTextHelpFormatter)
@@ -130,7 +133,6 @@ compQuality = args.compressionQuality
 tmpFile = '/tmp/batchIMv2.txt'
 devnull = open('/dev/null', 'w')
 
-                
 
 def timestampQuery(imageName):
     '''Function for returning the Image capturing time unix timestamp'''
@@ -185,7 +187,6 @@ def copyAllImages(srcDir):
             logger.info("Copying and Renaming Image : %s to %s" % (imageName, finalImage))
             shutil.copy2(imageName, finalImage)
 
-            
 
 imputCompString = "\n================================================================================================================\nPlease enter which directory images needs to be compressed and the quality level in the following format:\n\n\t\t\t\t*********************************\n\t\t\t\t  <DirectoyName_#qQualityLevel>\n\t\t\t\t*********************************\nEg.: Canon__Canon EOS 100D_#q90\n\nIn case you want to run compression on multiple directories please enter in csv format\nEg.: Canon__Canon EOS 100D_#q90, SAMSUNG__GT-I9100_q81\n\n================================================================================================================\n\nEnter your input: "
 
@@ -203,7 +204,6 @@ def validateUserCompressionInput():
         except ValueError as e:
             print(e)
 
-    
 
 def main():
     ## Start execution of the main program
@@ -212,9 +212,8 @@ def main():
     else:
         copyAllImages(sourceDir)
         validateUserCompressionInput()
-        
-    
 
-# Executing the script. 
+
+# Executing the script.
 if __name__ == "__main__":
     main()
