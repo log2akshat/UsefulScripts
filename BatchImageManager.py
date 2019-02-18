5b4efa8e123ccd627bee279a08380de6c8800539
log2akshat <akshat-pg8@iiitmk.ac.in>
2019-02-18 23:04:34 +0530

    Added missing-docstring for the script.

--- BatchImageManager.py
+++ BatchImageManager.py
@@ -14,6 +14,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""Script to run a compression only on specific camera make but not on others"""
 
 import os
 import time
