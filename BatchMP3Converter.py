5f62a91bce5d29cb283e68b466d7a8e62c1878f1
log2akshat <akshat-pg8@iiitmk.ac.in>
2019-06-26 15:33:27 +0530

    Added function is_valid_logging_status for
    checking the checking the valid log status.

--- BatchMP3Converter.py
+++ BatchMP3Converter.py
@@ -45,13 +45,19 @@ def is_target_directory(arg):
         # File exists so return the directory
         return arg
 	
+def is_valid_logging_status(parser, arg):
+    "Function for checking logging status is valid or not."
+    if not (arg == 'on' or arg == 'off'):
+        parser.error('{} is not a valid input for turning logging on or off! Please specify \"on\" for turning logging on and \"off\" for turning logging off.'.format(arg))
+    return arg
+
 	
 ## =========> Command line arguments parsing -- starts <========= ##
 parser = argparse.ArgumentParser(description='Batch mp4 to mp3 conversion utility. For running this program you need to have FFMPEG with mp3 codecs installed on your machine.')
 parser.add_argument('-s','--source_directory', help='Directory to read input video files.', required=True, metavar='<Source Directory>', type=lambda x: is_valid_directory(parser, x))
 parser.add_argument('-t','--target_directory', help='Directory to save output converted mp3 files.', required=True, metavar='<Target Directory>', type=lambda x: is_target_directory(x))
 parser.add_argument('-l','--log_file', help='Path of the log file.', metavar='<Log File>')
-parser.add_argument('-ls', '--logging_onoff', help='Logging status On/Off', metavar='<Logging on/off>', type=lambda x: is_valid_loggingStatus(parser, x))
+parser.add_argument('-ls', '--logging_onoff', help='Logging status On/Off', metavar='<Logging on/off>', type=lambda x: is_valid_logging_status(parser, x))
 args = parser.parse_args()
 
 ## =========> Command line arguments parsing -- ends <========= ##
