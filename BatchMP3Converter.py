0099688f81765b7b796163bdda6295aecb857144
log2akshat <akshat-pg8@iiitmk.ac.in>
2019-07-08 18:52:38 +0530

    Fixed missing-docstring as per pylint standards

--- BatchMP3Converter.py
+++ BatchMP3Converter.py
@@ -14,6 +14,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""Script for converting mp4 to mp3 in batch mode."""
 
 import os
 import sys
