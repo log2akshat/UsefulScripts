9175427833ad7bc75b7e75eca3c5eb4258c7498f
log2akshat <akshat-pg8@iiitmk.ac.in>
2020-02-02 23:24:43 +0530

    Converted to function.

--- ScreenShot.py
+++ ScreenShot.py
@@ -2,6 +2,13 @@
 
 import subprocess
 
-subprocess.call(['eog'])
-subprocess.call(['sleep', '2'])
-subprocess.call(['gnome-screenshot'])
+def main():
+    '''Call for taking Screenshots'''
+    subprocess.call(['eog'])
+    subprocess.call(['sleep', '2'])
+    subprocess.call(['gnome-screenshot'])
+
+
+# Executing the script.
+if __name__ == "__main__":
+    main()
