f4d0f6b89a56bd7783e9ad9e5efb76afde86c1f8
log2akshat <akshat-pg8@iiitmk.ac.in>
2022-03-29 22:53:58 +0530

    Fixed unused-variable warning as per pylint standards.

--- batch_mp3_converter.py
+++ batch_mp3_converter.py
@@ -40,6 +40,7 @@ def is_target_directory(parser, arg):
             subprocess.call(['mkdir', '-p', arg])
             return arg
         except subprocess.CalledProcessError as exp:
+            print(exp)
             parser.error('The directory {} does not exist and unable to create for you, please \
                          create it manually!'.format(arg))
     else:
