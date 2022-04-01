6c44ce7dbdc7d0d818af4e78323be133ecbedec0
log2akshat <akshat-pg8@iiitmk.ac.in>
2022-04-01 23:07:31 +0530

    Fixed line-too-long as per pylint standards

--- batch_mp3_converter.py
+++ batch_mp3_converter.py
@@ -103,7 +103,8 @@ CONSOLE_HANDLER = logging.StreamHandler(sys.stdout)
 CONSOLE_HANDLER.setLevel(logging.DEBUG)
 
 # create formatter
-FORMATTER = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+FORMATTER = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                               datefmt='%m/%d/%Y %I:%M:%S %p')
 
 # add formatter to handlers
 FILE_HANDLER.setFormatter(FORMATTER)
