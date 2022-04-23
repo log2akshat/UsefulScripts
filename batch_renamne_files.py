0ae3a0cf26d05d4131b5fe263aecf61449e73b72
log2akshat <akshat-pg8@iiitmk.ac.in>
2022-04-23 22:26:57 +0530

    Fixed line-too-long as per pylint standards

--- batch_renamne_files.py
+++ batch_renamne_files.py
@@ -37,7 +37,9 @@ def is_valid_logging_status(parser, arg):
 
 ## =========> Command line arguments parsing -- starts <========= ##
 PARSER = argparse.ArgumentParser(description='Batch renaming files utility...')
-PARSER.add_argument('-s', '--source_directory', help='Directory to read input files.', required=True, metavar='<Source Directory>', type=lambda x: is_valid_directory(PARSER, x))
+PARSER.add_argument('-s', '--source_directory', help='Directory to read input files.',
+                     required=True, metavar='<Source Directory>',
+                     type=lambda x: is_valid_directory(PARSER, x))
 PARSER.add_argument('-f', '--filename', help='Desired output file name.', required=True, metavar='<Output file names>')
 PARSER.add_argument('-l', '--log_file', help='Path of the log file.', metavar='<Log File>')
 PARSER.add_argument('-ls', '--logging_onoff', help='Logging status On/Off', metavar='<Logging on/off>', type=lambda x: is_valid_logging_status(PARSER, x))
