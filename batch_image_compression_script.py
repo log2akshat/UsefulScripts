80d32161cac8d41785586bb8e159d89da8396db1
log2akshat <akshat-pg8@iiitmk.ac.in>
2022-07-08 23:12:55 +0200

    Fixed line-too-long as per pylint standards

--- batch_image_compression_script.py
+++ batch_image_compression_script.py
@@ -59,9 +59,10 @@ PARSER.add_argument('-t', '--target_directory', help='Directory to save output f
                       type=lambda x: is_target_directory(PARSER, x))
 PARSER.add_argument('-q', '--quality', help='Quality of the Image to retain.', required=True,
                      metavar='<Image Quality>')
-PARSER.add_argument('-f', '--filename', help='Desired output file name.', metavar='<Output file names>')
+PARSER.add_argument('-f', '--filename', help='Desired output filename', metavar='<Output filenames>')
 PARSER.add_argument('-l', '--log_file', help='Path of the log file.', metavar='<Log File>')
-PARSER.add_argument('-ls', '--logging_onoff', help='Logging status On/Off', metavar='<Logging on/off>', type=lambda x: is_valid_logging_status(PARSER, x))
+PARSER.add_argument('-ls', '--logging_onoff', help='Logging status On/Off',
+                      metavar='<Logging on/off>', type=lambda x: is_valid_logging_status(PARSER, x))
 ARGS = PARSER.parse_args()
 
 ## =========> Command line arguments parsing -- ends <========= ##
