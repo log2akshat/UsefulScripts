5ca27adcde78d88be328b64f3efe7187c1871b2b
akshat <akshat-pg8@iiitmk.ac.in>
2022-08-28 23:07:21 +0200

    Fixed line-too-long as per pylint standards.

--- batch_image_manager.py
+++ batch_image_manager.py
@@ -43,7 +43,8 @@ def is_target_directory(parser, arg):
             subprocess.call(['mkdir', '-p', arg])
             return arg
         except IOError as ioexp:
-            parser.error('The directory {} does not exist and unable to create for you, please create it manually!'.format(arg), ioexp)
+            parser.error('The directory {} does not exist and unable to create for you, \
+                          please create it manually!'.format(arg), ioexp)
     return arg
 
 
