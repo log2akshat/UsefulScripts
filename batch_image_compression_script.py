7cbe19654848f78f095f305791437056b78146e1
akshat <akshat-pg8@iiitmk.ac.in>
2022-09-27 11:37:54 +0200

    Fixed bare-except as per pylint standards

--- batch_image_compression_script.py
+++ batch_image_compression_script.py
@@ -39,7 +39,7 @@ def is_target_directory(parser, arg):
         try:
             subprocess.call(['mkdir', '-p', arg])
             return arg
-        except:
+        except OSError:
             parser.error('The directory {} does not exist and unable to create for you, \
                           please create it manually!'.format(arg))
     # File exists so return the directory
