e5c6289f8cbf09cc0b58c673287a00a0b64f3229
akshat <akshat-pg8@iiitmk.ac.in>
2022-10-01 21:39:34 +0200

    Fixed inconsistent-return-statements as per pylint standards

--- batch_mp3_trimmer_converter.py
+++ batch_mp3_trimmer_converter.py
@@ -51,9 +51,8 @@ def is_target_directory(parser, arg):
             print(exp)
             parser.error('The directory {} does not exist and unable to create for you, \
                           please create it manually!'.format(arg))
-    else:
-        # File exists so return the directory
-        return arg
+    # File exists so return the directory
+    return arg
 
 
 def is_valid_logging_status(parser, arg):
