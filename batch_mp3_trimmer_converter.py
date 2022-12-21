fb5e06e4dff6b687058baa1be14958c692f4ffeb
akshat <akshat-pg8@iiitmk.ac.in>
2022-12-21 23:39:47 +0530

    batch_mp3_trimmer_converter.py: Added CLI option
    for or enabling/disabling quiet mode to suppress
    ffmpeg log messages.

--- batch_mp3_trimmer_converter.py
+++ batch_mp3_trimmer_converter.py
@@ -74,6 +74,8 @@ PARSER.add_argument('-s', '--source_directory', help='Directory to read input vi
 PARSER.add_argument('-t', '--target_directory', help='Directory to save converted mp3 files.',
                      required=True, metavar='<Target Directory>',
                      type=lambda x: is_target_directory(PARSER, x))
+PARSER.add_argument('-q', '--quiet_mode', help='Quiet mode On/Off', metavar='<Quiet mode on/off>',
+                    type=lambda x: is_valid_quiet_option(PARSER, x))
 PARSER.add_argument('-l', '--log_file', help='Path of the log file.', metavar='<Log File>')
 PARSER.add_argument('-ls', '--logging_onoff', help='Logging status On/Off',
                      metavar='<Logging on/off>', type=lambda x: is_valid_logging_status(PARSER, x))
