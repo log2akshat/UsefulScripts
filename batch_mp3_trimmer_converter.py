8e9d806e79869fe27e74082d8055dc7357717da9
akshat <akshat-pg8@iiitmk.ac.in>
2022-12-23 23:20:47 +0530

    batch_mp3_trimmer_converter.py:  Introduced logic
    for enabling/disabling quiet mode to supress
    ffmpeg log messages.

--- batch_mp3_trimmer_converter.py
+++ batch_mp3_trimmer_converter.py
@@ -134,9 +134,15 @@ def mp3_conversion(song_path, conversion_path, song_name):
     '''Function to convert the mp4 song to a mp3 file.'''
     song = conversion_path + "/" +  song_name + ".mp3"
     LOGGER.info("Going to convert %s song..", song)
-    cmd = subprocess.Popen(['ffmpeg', '-i', song_path, '-codec:a', 'libmp3lame', '-qscale:a',
-                            '2', conversion_path + "/" + song_name + '.mp3'],
-                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    if QUIET_MODE == 'off':
+        cmd = subprocess.Popen(['ffmpeg', '-i', song_path, '-codec:a', 'libmp3lame', '-qscale:a',
+                                '2', conversion_path + "/" + song_name + '.mp3'],
+                                 stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    else:
+        cmd = subprocess.Popen(['ffmpeg', '-hide_banner', '-loglevel', 'fatal', '-i', song_path,
+                                '-codec:a', 'libmp3lame', '-qscale:a',
+                                '2', conversion_path + "/" + song_name + '.mp3'],
+                                 stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     tee = subprocess.Popen(['tee', '-a', LOG_FILE], stdin=cmd.stdout)
     cmd.stdout.close()
     tee.communicate()
