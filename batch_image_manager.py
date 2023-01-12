b0cc62ada62258ea7dc78036935cd60f63f37b02
akshat <akshat-pg8@iiitmk.ac.in>
2023-01-12 22:17:56 +0530

    batch_image_manager.py: Reformatting parser error
    for is_valid_directory function.

--- batch_image_manager.py
+++ batch_image_manager.py
@@ -31,7 +31,7 @@ from argparse import RawTextHelpFormatter
 def is_valid_directory(parser, arg):
     "Function for checking specfied directory exists or not."
     if not os.path.isdir(arg):
-        parser.error('The directory {} does not exist!'.format(arg))
+        parser.error('\n\n\tThe directory {} does not exist!'.format(arg))
     return arg
 
 
