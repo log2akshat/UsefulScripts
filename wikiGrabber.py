c217ac72274a0af1db0f9161b1ff13c2c5ee4db8
akshat <akshat-pg8@iiitmk.ac.in>
2023-04-18 23:35:58 +0530

    wikiGrabber.py: Add logic to get user feedback
    interactively to grab the content from wiki
    and search the contents topics in wiki.

--- wikiGrabber.py
+++ wikiGrabber.py
@@ -1,7 +1,17 @@
 import wikipedia as wiki
 
-page_content = wiki.page("Jupiter").content
-print(page_content)
+print("Please select from the menu what you want to do..")
+print("Option 1: Type 1 if you want the content from the wiki for the specified keyword")
+print("Option 2: Type 2 if you want to search the content in the wiki for the specified keyword")
+selected_option=input("Please select your option: ")
 
-search_results = wiki.search("Mars")
-print(search_results)
+if (selected_option == str(1)):
+    page_content=input("Please enter the content you want from the wiki from the wiki: ")
+    grab_page_content = wiki.page(page_content).content
+    print(grab_page_content)
+elif (selected_option == str(2)):
+    search_page_content=input("Please enter the content you want to search in the wiki: ")
+    search_results = wiki.search(search_page_content)
+    print(search_results)
+else:
+    print(selected_option)
